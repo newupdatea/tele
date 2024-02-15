f9ba1f1913d75813ec522b27203135c77be4c8cb
newupdatea <88588471+newupdatea@users.noreply.github.com>
2024-02-16 00:14:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
-// تحديد الصفحات المختلفة هنا
-$pages = array(
-    'page1' => 'page1.php',
-    'page2' => 'page2.php',
-    'page3' => 'page3.php'
+// تحديد الروابط المختلفة هنا
+$links = array(
+    'link1' => 'https://example.com/page1',
+    'link2' => 'https://example.com/page2',
+    'link3' => 'https://example.com/page3'
 );
 
 // التحقق من الضغطة التي قام بها المستخدم
-if (isset($_GET['page'])) {
-    $selectedPage = $_GET['page'];
-    if (array_key_exists($selectedPage, $pages)) {
-        $redirectPage = $pages[$selectedPage];
-        header("Location: $redirectPage");
+if (isset($_GET['link'])) {
+    $selectedLink = $_GET['link'];
+    if (array_key_exists($selectedLink, $links)) {
+        $redirectLink = $links[$selectedLink];
+        header("Location: $redirectLink");
         exit();
     }
 }
