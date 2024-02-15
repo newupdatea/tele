ad2279ee5751aa6c08f4eca1e216a6cb136e2752
newupdatea <88588471+newupdatea@users.noreply.github.com>
2024-02-16 00:13:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<?php
+// تحديد الصفحات المختلفة هنا
+$pages = array(
+    'page1' => 'page1.php',
+    'page2' => 'page2.php',
+    'page3' => 'page3.php'
+);
 
+// التحقق من الضغطة التي قام بها المستخدم
+if (isset($_GET['page'])) {
+    $selectedPage = $_GET['page'];
+    if (array_key_exists($selectedPage, $pages)) {
+        $redirectPage = $pages[$selectedPage];
+        header("Location: $redirectPage");
+        exit();
+    }
+}
+?>
