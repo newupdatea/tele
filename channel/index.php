d2fd0db9fccf9e1e7f901ea80d2677740371c825
newupdatea <88588471+newupdatea@users.noreply.github.com>
2024-02-16 00:18:13 +0300

    Update index.php

--- channel/index.php
+++ channel/index.php
@@ -1,1 +1,18 @@
+<?php
 
+$links = array(
+    'link1' => 'https://example.com/page1',
+    'link2' => 'https://example.com/page2',
+    'link3' => 'https://example.com/page3'
+);
+
+
+if (isset($_GET['link'])) {
+    $selectedLink = $_GET['link'];
+    if (array_key_exists($selectedLink, $links)) {
+        $redirectLink = $links[$selectedLink];
+        header("Location: $redirectLink");
+        exit();
+    }
+}
+?>
